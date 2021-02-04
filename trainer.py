778093dc93537583246ec4bf56c6c81348fc6a77
Matt <matthew.poyser@durham.ac.uk>
2021-02-04 14:30:01 +0000

    debug trainer

--- trainer.py
+++ trainer.py
@@ -56,7 +56,6 @@ def train_epoch(train_loader, model, loss_fn, optimizer, cuda, log_interval, met
 
 
         optimizer.zero_grad()
-        raise AttributeError(*data)
         outputs = model(*data)
 
         if type(outputs) not in (tuple, list):
